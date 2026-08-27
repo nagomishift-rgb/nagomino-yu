9f03a9198fffb827b564a15b669f4b6a36565ac5
nagomishift-rgb <nagomi.shift@gmail.com>
2026-08-27 20:38:27 +0900

    Delete quiz.mjs

--- quiz.mjs
+++
@@ -1,5 +0,0 @@
-import {quiz} from "./quiz-data.mjs";
-export default async()=>Response.json(
-  quiz.map(({id,question,options})=>({id,question,options})),
-  {headers:{"Cache-Control":"no-store"}}
-);
